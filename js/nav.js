3a1cc04c7fafe942eaa51fefde6a45dfbe39ed21
Alvaro1899 <martinez.alvaro61@gmail.com>
2024-09-14 01:19:15 -0600

    Actualizacion 14/9/24

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,13 @@
 function openNav(){
     document.getElementById("mobile-menu").style.width = "100%";
 }
-
 function closeNav(){
     document.getElementById("mobile-menu").style.width = "0%";
+}
+function openPubl() {
+    document.getElementById("mobile-publ").style.width = "85%"; // Ajusta el ancho al 50%
+}
+
+function closePubl() {
+    document.getElementById("mobile-publ").style.width = "0"; // Vuelve a ocultar el cuadro
 }
